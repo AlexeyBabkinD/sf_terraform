a7a267ed6f9b420fe9d9ac75a8b5d56504faf0b3
Alexey Babkin <alexey@mypc>
2022-11-04 18:00:30 +0300

    added LB

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
     bucket     = "tf-state-bucket-alexey"
     region     = "ru-central1"
     key        = "terrastate/sf_alexey.tfstate"
-    access_key = "<my-key-id>"
-    secret_key = "<my-secret-key>"
+    access_key = "YCAJEm1uVb2CZ5S4wU_5I9GO_"
+    secret_key = "YCOToW5AXg3U74L_092NGp3Mo0ZVB5Rg5D9zJmwy"
 
     skip_region_validation      = true
     skip_credentials_validation = true
@@ -23,6 +23,7 @@ provider "yandex" {
   cloud_id                 = "aje5tbqlasnl7oj52t43"
   folder_id                = "b1g47hvi0pim20769n95"
 }
+
 
 resource "yandex_vpc_network" "network" {
   name = "network"
@@ -56,3 +57,43 @@ module "ya_instance_2" {
   vpc_subnet_id         = yandex_vpc_subnet.subnet2.id
   vpc_subnet_zone       = yandex_vpc_subnet.subnet2.zone
 }
+
+resource "yandex_lb_target_group" "my_tg" {
+  name = "my-target-group"
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    address   = module.ya_instance_1.internal_ip_address_vm
+  }
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet2.id
+    address   = module.ya_instance_2.internal_ip_address_vm
+  }
+
+}
+
+resource "yandex_lb_network_load_balancer" "my_lb" {
+  name = "my-network-load-balancer"
+
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.my_tg.id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
+}
+
