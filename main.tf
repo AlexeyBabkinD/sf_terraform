be77e5ccff32b2a15934ce07791f7af909061a1a
Alexey Babkin <115026581+AlexeyBabkinD@users.noreply.github.com>
2022-11-04 18:02:36 +0300

    Terraform conf (#1)
    * added terraform
    
    * added LB
    
    * added LB
    
    Co-authored-by: Alexey Babkin <alexey@mypc>

---
+++ main.tf
@@ -0,0 +1,99 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "tf-state-bucket-alexey"
+    region     = "ru-central1"
+    key        = "terrastate/sf_alexey.tfstate"
+    access_key = "<my_bucket_key_id>"
+    secret_key = "<my_bucket_secret_key>"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = file("~/key.json")
+  cloud_id                 = "aje5tbqlasnl7oj52t43"
+  folder_id                = "b1g47hvi0pim20769n95"
+}
+
+
+resource "yandex_vpc_network" "network" {
+  name = "network"
+}
+
+resource "yandex_vpc_subnet" "subnet1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet2" {
+  name           = "subnet2"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.11.0/24"]
+}
+
+
+module "ya_instance_1" {
+  source                = "./modules/instance"
+  instance_family_image = "lemp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
+  vpc_subnet_zone       = yandex_vpc_subnet.subnet1.zone
+}
+
+module "ya_instance_2" {
+  source                = "./modules/instance"
+  instance_family_image = "lamp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet2.id
+  vpc_subnet_zone       = yandex_vpc_subnet.subnet2.zone
+}
+
+resource "yandex_lb_target_group" "my_tg" {
+  name = "my-target-group"
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet1.id
+    address   = module.ya_instance_1.internal_ip_address_vm
+  }
+
+  target {
+    subnet_id = yandex_vpc_subnet.subnet2.id
+    address   = module.ya_instance_2.internal_ip_address_vm
+  }
+
+}
+
+resource "yandex_lb_network_load_balancer" "my_lb" {
+  name = "my-network-load-balancer"
+
+  listener {
+    name = "my-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.my_tg.id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
+}
+
