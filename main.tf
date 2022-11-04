2f9b421a99967390beb8c3636c50fb9dca375ffa
Alexey Babkin <alexey@mypc>
2022-11-04 16:16:30 +0300

    added terraform

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "tf-state-bucket-alexey"
+    region     = "ru-central1"
+    key        = "terrastate/sf_alexey.tfstate"
+    access_key = "<my-key-id>"
+    secret_key = "<my-secret-key>"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = file("~/key.json")
+  cloud_id                 = "aje5tbqlasnl7oj52t43"
+  folder_id                = "b1g47hvi0pim20769n95"
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "network"
+}
+
+resource "yandex_vpc_subnet" "subnet1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet2" {
+  name           = "subnet2"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.11.0/24"]
+}
+
+
+module "ya_instance_1" {
+  source                = "./modules/instance"
+  instance_family_image = "lemp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet1.id
+  vpc_subnet_zone       = yandex_vpc_subnet.subnet1.zone
+}
+
+module "ya_instance_2" {
+  source                = "./modules/instance"
+  instance_family_image = "lamp"
+  vpc_subnet_id         = yandex_vpc_subnet.subnet2.id
+  vpc_subnet_zone       = yandex_vpc_subnet.subnet2.zone
+}
